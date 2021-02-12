edb76ddb40ac8a4502fdff65a1f5d7c54738ff66
Taylor Tokareff <taylortokareff@gmail.com>
2021-02-12 15:45:12 -0800

    finishing function

--- dom-utilities.js
+++ dom-utilities.js
@@ -4,27 +4,27 @@ export function sync(input, span) {
 
 function madLibs(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, output1, output2, output3, output4, output5, output6, output7, output8, output9, output10) {
 
-const word1 = document.getElementById(input1);
-const word2 = document.getElementById(input2);
-const word3 = document.getElementById(input3);
-const word4 = document.getElementById(input4);
-const word5 = document.getElementById(input5);
-const word6 = document.getElementById(input6);
-const word7 = document.getElementById(input7);
-const word8 = document.getElementById(input8);
-const word9 = document.getElementById(input9);
-const word10 = document.getElementById(input10);
-const span1 = document.getElementById(output1);
-const span2 = document.getElementById(output2);
-const span3 = document.getElementById(output3);
-const span4 = document.getElementById(output4);
-const span5 = document.getElementById(output5);
-const span6 = document.getElementById(output6);
-const span7 = document.getElementById(output7);
-const span8 = document.getElementById(output8);
-const span9 = document.getElementById(output9);
-const span10 = document.getElementById(output10);
-const button = document.getElementById(madlib-button);
+const word1 = document.getElementById('input1');
+const word2 = document.getElementById('input2');
+const word3 = document.getElementById('input3');
+const word4 = document.getElementById('input4');
+const word5 = document.getElementById('input5');
+const word6 = document.getElementById('input6');
+const word7 = document.getElementById('input7');
+const word8 = document.getElementById('input8');
+const word9 = document.getElementById('input9');
+const word10 = document.getElementById('input10');
+const span1 = document.getElementById('output1');
+const span2 = document.getElementById('output2');
+const span3 = document.getElementById('output3');
+const span4 = document.getElementById('output4');
+const span5 = document.getElementById('output5');
+const span6 = document.getElementById('output6');
+const span7 = document.getElementById('output7');
+const span8 = document.getElementById('output8');
+const span9 = document.getElementById('output9');
+const span10 = document.getElementById('output10');
+const button = document.getElementById('madlibButton');
 const expected1 = "";
 const expected2 = "";
 const expected3 = "";
@@ -46,4 +46,8 @@ input8.value = expected8;
 input9.value = expected9;
 input10.value = expected10;
 
-}
+madlibButton.addEventListener('click', () => {
+    sync(word1, span1);
+    
+});
+
