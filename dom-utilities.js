67adeb9777fb584aed5181d6dfcce6a80229ba93
Taylor Tokareff <taylortokareff@gmail.com>
2021-02-12 14:52:34 -0800

    worked on function

--- dom-utilities.js
+++ dom-utilities.js
@@ -2,4 +2,25 @@ export function sync(input, span) {
     return span.textContent = input.value;
 }
 
-function madLibs(input, input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16);
+function madLibs(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16) {
+
+const word2 = document.getElementById(input1);
+const word3 = document.getElementById(input2);
+const word4 = document.getElementById(input3);
+const word5 = document.getElementById(input4);
+const word6 = document.getElementById(input5);
+const word7 = document.getElementById(input6);
+const word8 = document.getElementById(input7);
+const word9 = document.getElementById(input8);
+const word10 = document.getElementById(input9);
+const word11 = document.getElementById(input10);
+const word12 = document.getElementById(input11);
+const word13 = document.getElementById(input12);
+const word14 = document.getElementById(input13);
+const word15 = document.getElementById(input14);
+const word16 = document.getElementById(input15);
+const word17 = document.getElementById(input16);
+const button = document.getElementById(madlib-button);
+ 
+
+}
