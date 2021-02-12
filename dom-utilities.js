7b70eea39e7eeb03fb090797a899ace20dcc9482
BenFrancka <benfrancka@gmail.com>
2021-02-12 15:25:41 -0800

    crazy long function

--- dom-utilities.js
+++ dom-utilities.js
@@ -2,7 +2,7 @@ export function sync(input, span) {
     return span.textContent = input.value;
 }
 
-function madLibs(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16) {
+function madLibs(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, output1, output2, output3, output4, output5, output6, output7, output8, output9, output10) {
 
 const word1 = document.getElementById(input1);
 const word2 = document.getElementById(input2);
@@ -14,17 +14,36 @@ const word7 = document.getElementById(input7);
 const word8 = document.getElementById(input8);
 const word9 = document.getElementById(input9);
 const word10 = document.getElementById(input10);
-const span1 = document.getElementById(input11);
-const span2 = document.getElementById(input12);
-const span3 = document.getElementById(input13);
-const span4 = document.getElementById(input14);
-const span5 = document.getElementById(input15);
-const span6 = document.getElementById(input16);
-const span7 = document.getElementById(input16);
-const span8 = document.getElementById(input16);
-const span9 = document.getElementById(input16);
-const span10 = document.getElementById(input16);
+const span1 = document.getElementById(output1);
+const span2 = document.getElementById(output2);
+const span3 = document.getElementById(output3);
+const span4 = document.getElementById(output4);
+const span5 = document.getElementById(output5);
+const span6 = document.getElementById(output6);
+const span7 = document.getElementById(output7);
+const span8 = document.getElementById(output8);
+const span9 = document.getElementById(output9);
+const span10 = document.getElementById(output10);
 const button = document.getElementById(madlib-button);
-input.value =  
+const expected1 = "";
+const expected2 = "";
+const expected3 = "";
+const expected4 = "";
+const expected5 = "";
+const expected6 = "";
+const expected7 = "";
+const expected8 = "";
+const expected9 = "";
+const expected10= "";
+input1.value = expected1;
+input2.value = expected2;
+input3.value = expected3;
+input4.value = expected4;
+input5.value = expected5;
+input6.value = expected6;
+input7.value = expected7;
+input8.value = expected8;
+input9.value = expected9;
+input10.value = expected10;
 
 }
