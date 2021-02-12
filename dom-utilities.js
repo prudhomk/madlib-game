267a504de46139d666b76ceea471b10c549711a3
Kalan Prudhomme <cleaver9@gmail.com>
2021-02-12 15:08:47 -0800

    span restructure

--- dom-utilities.js
+++ dom-utilities.js
@@ -4,23 +4,27 @@ export function sync(input, span) {
 
 function madLibs(input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15, input16) {
 
-const word2 = document.getElementById(input1);
-const word3 = document.getElementById(input2);
-const word4 = document.getElementById(input3);
-const word5 = document.getElementById(input4);
-const word6 = document.getElementById(input5);
-const word7 = document.getElementById(input6);
-const word8 = document.getElementById(input7);
-const word9 = document.getElementById(input8);
-const word10 = document.getElementById(input9);
-const word11 = document.getElementById(input10);
-const word12 = document.getElementById(input11);
-const word13 = document.getElementById(input12);
-const word14 = document.getElementById(input13);
-const word15 = document.getElementById(input14);
-const word16 = document.getElementById(input15);
-const word17 = document.getElementById(input16);
+const word1 = document.getElementById(input1);
+const word2 = document.getElementById(input2);
+const word3 = document.getElementById(input3);
+const word4 = document.getElementById(input4);
+const word5 = document.getElementById(input5);
+const word6 = document.getElementById(input6);
+const word7 = document.getElementById(input7);
+const word8 = document.getElementById(input8);
+const word9 = document.getElementById(input9);
+const word10 = document.getElementById(input10);
+const span1 = document.getElementById(input11);
+const span2 = document.getElementById(input12);
+const span3 = document.getElementById(input13);
+const span4 = document.getElementById(input14);
+const span5 = document.getElementById(input15);
+const span6 = document.getElementById(input16);
+const span7 = document.getElementById(input16);
+const span8 = document.getElementById(input16);
+const span9 = document.getElementById(input16);
+const span10 = document.getElementById(input16);
 const button = document.getElementById(madlib-button);
- 
+input.value =  
 
 }
